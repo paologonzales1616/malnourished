9f17c03894407a77132007fb0c3147ef46d6cb2d
paolo gonzales <paologonzales1616@gmail.com>
2020-01-11 11:49:15 +0800

    Added react client

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log("Server is running on port " + port);
