d76afbe2ce244f969c4e7bada956460fe1940e3c
paolo gonzales <paologonzales1616@gmail.com>
2020-01-11 13:13:30 +0800

    Client gitignore for production

---
+++ client/build/precache-manifest.58b6ae8c06ada625b2b401d8438e4432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f674bef98b286a77c7629c84219252b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "255b9a9de5f8b9312d55",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "01e0a37065288daab2a0",
+    "url": "/static/js/2.05d89e5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.05d89e5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "255b9a9de5f8b9312d55",
+    "url": "/static/js/main.3ced95f4.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
